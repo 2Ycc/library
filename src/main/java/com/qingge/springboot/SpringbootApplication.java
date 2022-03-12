88197a2be16ae37676acb7b6cae908f3dbf11d82
2Ycc <1109926649@qq.com>
2022-03-12 14:50:17 +0800

    修改文件上传路径

--- src/main/java/com/qingge/springboot/SpringbootApplication.java
+++ src/main/java/com/qingge/springboot/SpringbootApplication.java
@@ -3,6 +3,9 @@ package com.qingge.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Closer
+ */
 @SpringBootApplication
 public class SpringbootApplication {
 
